58a64bce8d0581720e07451dd4c44c90fc3a5b7b
HENRIQUE CRUZ <henriquecruz1986@gmail.com>
2021-09-10 21:55:36 -0300

    Projeto em Construção

---
+++ src/test/java/projetosalareunioes/saladereuniao/SaladereuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package projetosalareunioes.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
